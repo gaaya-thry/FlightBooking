8a7b7b265894c8cdba66f672bb3308d7abcfb1de
gaaya-thry <154420538+gaaya-thry@users.noreply.github.com>
2024-11-25 19:56:22 +0800

    Update Booking_System.java

--- Booking_System.java
+++ Booking_System.java
@@ -3,4 +3,25 @@
 //user search the flight by Date
 
 public class Booking_System{
+
+  public class Payment {
+    public static void choosePaymentMethod() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a payment method:");
+        System.out.println("1. Visa");
+        System.out.println("2. MasterCard");
+        System.out.println("3. PayPal");
+        System.out.println("4. Other");
+        
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 -> System.out.println("You chose Visa.");
+            case 2 -> System.out.println("You chose MasterCard.");
+            case 3 -> System.out.println("You chose PayPal.");
+            case 4 -> System.out.println("You chose Other.");
+            default -> System.out.println("Invalid choice.");
+        }
+    }
+}
+
 }
